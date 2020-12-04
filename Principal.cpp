5f185122b2f763dcafd8f3f53bbd4605df53f63e
Alex Reinoso <ralexjessiel@gmail.com>
2020-12-04 13:53:34 -0500

    carga imagenes

--- Principal.cpp
+++ Principal.cpp
@@ -3,8 +3,12 @@
 
 int main(int argc, char *arv[]){
 
-    
+    Mat imagen = imread("imagenes/1.jpg");
 
+    namedWindow("Imagen", WINDOW_AUTOSIZE);
+    imshow("Imagen", imagen);
+    waitKey(0);
+    destroyAllWindows();
 
     return 0;
 }
